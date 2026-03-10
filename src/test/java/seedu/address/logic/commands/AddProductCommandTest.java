661237a1313d93b26912703ebcc67cde05f930d1
Le Hua <l3hu4l1@gmail.com>
2026-03-10 22:43:13 +0800

    Create AddProductCommandTest.java

---
+++ src/test/java/seedu/address/logic/commands/AddProductCommandTest.java
@@ -0,0 +1,260 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_IDENTIFIER_WARN;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_PRODUCT_NAME_WARN;
+import static seedu.address.logic.parser.ParserUtil.NEWLINE;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyInventory;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.ReadOnlyVendorVault;
+import seedu.address.model.person.Person;
+import seedu.address.model.product.Identifier;
+import seedu.address.model.product.Name;
+import seedu.address.model.product.Product;
+import seedu.address.testutil.ProductBuilder;
+
+public class AddProductCommandTest {
+    @Test
+    public void constructor_nullProduct_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddProductCommand(null));
+    }
+
+    @Test
+    public void execute_productAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingProductAdded modelStub = new ModelStubAcceptingProductAdded();
+        Product validProduct = new ProductBuilder().build();
+
+        CommandResult commandResult = new AddProductCommand(validProduct).execute(modelStub);
+
+        assertEquals(String.format(AddProductCommand.MESSAGE_SUCCESS, Messages.formatProduct(validProduct)),
+                commandResult.getFeedbackToUser());
+        assertEquals(CommandResult.FEEDBACK_TYPE_SUCCESS, commandResult.getFeedbackType());
+        assertEquals(1, modelStub.productsAdded.size());
+        assertEquals(validProduct, modelStub.productsAdded.get(0));
+    }
+
+    @Test
+    public void execute_withWarnings_success() throws Exception {
+        ModelStubAcceptingProductAdded modelStub = new ModelStubAcceptingProductAdded();
+
+        Product validProductWithWarnings = new ProductBuilder()
+                .withIdentifier(INVALID_IDENTIFIER_WARN)
+                .withName(INVALID_PRODUCT_NAME_WARN)
+                .build();
+
+        String warnings = Identifier.MESSAGE_WARN + NEWLINE + Name.MESSAGE_WARN;
+        AddProductCommand addProductCommand = new AddProductCommand(validProductWithWarnings, warnings);
+
+        CommandResult result = addProductCommand.execute(modelStub);
+
+        String expectedMessage = String.format(AddProductCommand.MESSAGE_SUCCESS + NEWLINE + warnings,
+                Messages.formatProduct(validProductWithWarnings));
+
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+        assertEquals(CommandResult.FEEDBACK_TYPE_WARN, result.getFeedbackType());
+    }
+
+    @Test
+    public void getPendingConfirmation_returnsInactivePendingConfirmation() {
+        Product validProduct = new ProductBuilder().build();
+        AddProductCommand addProductCommand = new AddProductCommand(validProduct);
+
+        PendingConfirmation pendingConfirmation = addProductCommand.getPendingConfirmation();
+        assertFalse(pendingConfirmation.getNeedConfirmation());
+    }
+
+    @Test
+    public void equals() {
+        Product productA = new ProductBuilder().withIdentifier("SKU-A").build();
+        Product productB = new ProductBuilder().withIdentifier("SKU-B").build();
+        AddProductCommand addProductA = new AddProductCommand(productA);
+        AddProductCommand addProductB = new AddProductCommand(productB);
+
+        // same object -> returns true
+        assertTrue(addProductA.equals(addProductA));
+
+        // same values -> returns true
+        assertTrue(addProductA.equals(new AddProductCommand(productA)));
+
+        // different types -> returns false
+        assertFalse(addProductA.equals(1));
+
+        // null -> returns false
+        assertFalse(addProductA.equals(null));
+
+        // different product -> returns false
+        assertFalse(addProductA.equals(addProductB));
+    }
+
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook addressBook) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setInventory(ReadOnlyInventory inventory) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyInventory getInventory() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setVendorVault(ReadOnlyVendorVault vendorVault) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyVendorVault getVendorVault() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void archivePerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void restorePerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasProduct(Product product) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteProduct(Product target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addProduct(Product product) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setProduct(Product target, Product editedProduct) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Product> getFilteredProductList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredProductList(Predicate<Product> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void commitVendorVault() {
+        }
+
+        @Override
+        public void undoVendorVault() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean canUndoVendorVault() {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    private class ModelStubAcceptingProductAdded extends ModelStub {
+        final ArrayList<Product> productsAdded = new ArrayList<>();
+
+        @Override
+        public void addProduct(Product product) {
+            requireNonNull(product);
+            productsAdded.add(product);
+        }
+    }
+}
