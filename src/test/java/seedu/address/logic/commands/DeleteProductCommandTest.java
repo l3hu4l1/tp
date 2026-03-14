256e722ed8d5724109cc52b355b1a743af7c308f
Shermaine <pehshermaine@gmail.com>
2026-03-14 23:36:05 +0800

    style: fix checkstyle

--- src/test/java/seedu/address/logic/commands/DeleteProductCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteProductCommandTest.java
@@ -3,8 +3,8 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
