80969d8fc7e512e309518b1e10dcb6245e14b824
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-03-22 16:30:07 +0800

    Add test cases for FindProductCommand.java

---
+++ src/test/java/seedu/address/logic/commands/FindProductCommandTest.java
@@ -0,0 +1,97 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.Messages.MESSAGE_PRODUCTS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalProducts.EGGS;
+import static seedu.address.testutil.TypicalProducts.OIL;
+import static seedu.address.testutil.TypicalProducts.RICE;
+import static seedu.address.testutil.TypicalProducts.getTypicalInventory;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Aliases;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.VendorVault;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.product.ProductNameContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code FindProductCommand}.
+ */
+public class FindProductCommandTest {
+    private Model model = new ModelManager(new VendorVault(
+            getTypicalAddressBook(), getTypicalInventory()), new UserPrefs(), new Aliases());
+    private Model expectedModel = new ModelManager(new VendorVault(
+            getTypicalAddressBook(), getTypicalInventory()), new UserPrefs(), new Aliases());
+
+    @Test
+    public void equals() {
+        ProductNameContainsKeywordsPredicate firstPredicate =
+                new ProductNameContainsKeywordsPredicate(Collections.singletonList("first"));
+        ProductNameContainsKeywordsPredicate secondPredicate =
+                new ProductNameContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        FindProductCommand findFirstCommand = new FindProductCommand(firstPredicate);
+        FindProductCommand findSecondCommand = new FindProductCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        FindProductCommand findFirstCommandCopy = new FindProductCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noProductFound() {
+        String expectedMessage = String.format(MESSAGE_PRODUCTS_LISTED_OVERVIEW, 0);
+        ProductNameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        FindProductCommand command = new FindProductCommand(predicate);
+        expectedModel.updateFilteredProductList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredProductList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PRODUCTS_LISTED_OVERVIEW, 3);
+        ProductNameContainsKeywordsPredicate predicate = preparePredicate("Rice Oil Eggs");
+        FindProductCommand command = new FindProductCommand(predicate);
+        expectedModel.updateFilteredProductList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(RICE, OIL, EGGS), model.getFilteredProductList());
+    }
+
+    @Test
+    public void getPendingConfirmation_returnsInactivePendingConfirmation() {
+        ProductNameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        FindProductCommand command = new FindProductCommand(predicate);
+        PendingConfirmation pendingConfirmation = command.getPendingConfirmation();
+        assertFalse(pendingConfirmation.getNeedConfirmation());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code ProductNameContainsKeywordsPredicate}.
+     */
+    private ProductNameContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new ProductNameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+}
