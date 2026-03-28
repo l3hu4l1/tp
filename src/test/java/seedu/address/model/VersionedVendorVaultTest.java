682eeef2aa0ad1f47ed54d6184bda650580afd49
Jolene <jolenechong7@gmail.com>
2026-03-28 15:16:38 +0800

    fix: Resolve checkstyle issues

--- src/test/java/seedu/address/model/VersionedVendorVaultTest.java
+++ src/test/java/seedu/address/model/VersionedVendorVaultTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
