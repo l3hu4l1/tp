3ed2c5d58d05b8b0a8e8900dd169b7fa0d3dd5b8
Le Hua <l3hu4l1@gmail.com>
2026-04-01 08:27:42 +0800

    Update Messages.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -15,7 +15,7 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_MISSING_PREFIX = "Missing required field(s): ";
+    public static final String MESSAGE_MISSING_PREFIX = "Missing required prefix(es): ";
     public static final String MESSAGE_NON_PREFIX_BEFORE_PREFIX = "No non-prefix characters before "
             + "prefix(es) is allowed, ";
     public static final String MESSAGE_MISSING_FIELD_FORMAT = "%s (%s)";
