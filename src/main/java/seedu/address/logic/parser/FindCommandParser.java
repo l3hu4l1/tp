cdfea3b045eedfdbc604131ee9aa67bd0af77692
Le Hua <l3hu4l1@gmail.com>
2026-04-05 16:48:13 +0800

    Extend parser with tag mode

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -3,20 +3,23 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsScoredPredicate;
+import seedu.address.model.person.PersonTagContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
+    private static final String TAG_MODE_FLAG = "-t";
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
@@ -25,9 +28,23 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        List<String> tokens = Arrays.asList(trimmedArgs.split("\\s+"));
+        boolean isTagMode = TAG_MODE_FLAG.equals(tokens.get(0)); // first token must be -t
 
-        return new FindCommand(new NameContainsKeywordsScoredPredicate(Arrays.asList(nameKeywords)));
+        boolean hasNonLeadingTag = !isTagMode && tokens.subList(1, tokens.size()).contains(TAG_MODE_FLAG);
+        if (hasNonLeadingTag) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        if (isTagMode) {
+            if (tokens.size() == 1) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
+
+            return new FindCommand(new PersonTagContainsKeywordsPredicate(tokens.subList(1, tokens.size())));
+        }
+
+        return new FindCommand(new NameContainsKeywordsScoredPredicate(tokens));
     }
 
 }
